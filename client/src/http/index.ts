63cc3180d36895f4539fe1a14d79de2ffa8484b7
HnatKruk <hnatkivpublic@gmail.com>
2024-02-01 17:55:48 +0100

    Update gitignore and vercel files

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { AuthResponse } from '../models/AuthResponse'
 
-export const API_URL = `http://localhost:4000/api`
+export const API_URL = process.env.API_URL
 
 const $api = axios.create({
   withCredentials: true,
